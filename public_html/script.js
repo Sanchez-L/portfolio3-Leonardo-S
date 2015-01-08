dd6266fb7b43a4b63882591854ccecc9af8ca489
leonardo sanchez <leonardodicaprip831@yahoo.ocm>
2015-01-08 08:44:08 -0800

    i have started on my portfolio 3

---
+++ public_html/script.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+console.log("script");
+
+$("document").ready(function() {
+    $(".whatever").css("background-color", "yellow");
+});
+
+
