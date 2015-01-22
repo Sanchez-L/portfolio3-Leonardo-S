2a88d51159b06971556eb31153c475df27daaee5
leonardo sanchez <leonardodicaprip831@yahoo.ocm>
2015-01-22 09:21:32 -0800

    1/22/15

--- public_html/script.js
+++ public_html/script.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//this whole function helps jquery to make my page better looking
 $("document").ready(function() {
 
 
@@ -11,8 +11,7 @@ $("document").ready(function() {
     $('.jumbotron').css('background-image', ' url(http://www.metalinjection.net/wp-content/uploads/2014/07/space-metal.jpg ) no-repeat center center');
 
     //tells gogle that a button was pressed.
-    $('#onebutton').bind('click', alertButtonClick);
-
+    $('#onebutton').bind('click', alertButtonClick);   
 
     //makes my project slide away from my page
     $('#hide').bind('click', hideThePage);
@@ -25,9 +24,11 @@ $("document").ready(function() {
 
     //changes my paragraph color when mouse is over
     $('#p').bind('mouseover', 'mouseoverme').bind('mouseout', 'mouseoutme');
-    
-    $('#accordion').css("background-color", "grey");
 
+    //this is for my auto accordion
+    $('.ui-widget-header').css("background", "black");
+
+    $('.2').css("opcacity", '0.5');
 });
 
 //this is an accordion and it works as a dropdown
@@ -46,82 +47,12 @@ function alertButtonClick() {
     alert("you clicked my button");
 }
 
-//helps the text change when mouse is over
-function mouseoverme() {
-    $("#second").html("no touching");
-    $('#p').hide('flip', {}, 2500);
-
-}
-
-//helps text turn back its color when mouse is out
-function mouseoutme() {
-    $("#second").html("cant you read i said dont touch that");
-    $('#p').show('flip', {}, 2500);
-    
-    }
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
-  $(function() {
+//this helps me select the date date picker 
+$(function() {
+    $("#datepicker").datepicker();
+});
+$(function() {
     $("#accordion").accordion({
         event: "click hoverintent"
     });
 });
-$.event.special.hoverintent = {
-    setup: function() {
-        $(this).bind("mouseover", jQuery.event.special.hoverintent.handler);
-    },
-    teardown: function() {
-        $(this).unbind("mouseover", jQuery.event.special.hoverintent.handler);
-    },
-    handler: function(event) {
-        var currentX, currentY, timeout,
-                args = arguments,
-                target = $(event.target),
-                previousX = event.pageX,
-                previousY = event.pageY;
-
-        function track(event) {
-            currentX = event.pageX;
-            currentY = event.pageY;
-        }
-        ;
-
-        function clear() {
-            target
-                    .unbind("mousemove", track)
-                    .unbind("mouseout", clear);
-            clearTimeout(timeout);
-        }
-
-        function handler() {
-            var prop,
-                    orig = event;
-
-            if ((Math.abs(previousX - currentX) +
-                    Math.abs(previousY - currentY)) < 7) {
-                clear();
-
-                event = $.Event("hoverintent");
-                for (prop in orig) {
-                    if (!(prop in event)) {
-                        event[ prop ] = orig[ prop ];
-                    }
-                }
-                delete event.originalEvent;
-
-                target.trigger(event);
-            } else {
-                previousX = currentX;
-                previousY = currentY;
-                timeout = setTimeout(handler, 100);
-            }
-        }
-
-        timeout = setTimeout(handler, 100);
-        target.bind({
-            mousemove: track,
-            mouseout: clear
-        });
-    }
-};
